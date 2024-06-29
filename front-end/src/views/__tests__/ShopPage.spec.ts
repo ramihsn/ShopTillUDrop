6968e6910a0787f09d3e2061328ba5d884486b71
Rami Hassan <hsn.rami@gmail.com>
2024-06-29 16:24:21 +0300

    upgrade unit test for shpe page

--- front-end/src/views/__tests__/ShopPage.spec.ts
+++ front-end/src/views/__tests__/ShopPage.spec.ts
@@ -1,31 +1,19 @@
 import { describe, it, expect, vi } from 'vitest';
 import { shallowMount } from '@vue/test-utils';
 import ShopPage from '../ShopPage.vue';
-import { products } from '@/mockData';
 
 describe('ShopPage.vue', () => {
-    it('renders a list of products', () => {
-        const wrapper = shallowMount(ShopPage);
-        const productItems = wrapper.findAll('.product-item');
-        expect(productItems.length).toBe(products.length);
-    });
+  it('calls addToCart method when button is clicked', async () => {
+    const wrapper = shallowMount(ShopPage);
+    const addToCartSpy = vi.spyOn(wrapper.vm, 'addToCart');
 
-    it('renders product details correctly', () => {
-        const wrapper = shallowMount(ShopPage);
-        const firstProduct = products[0];
-        const firstProductItem = wrapper.find('.product-item');
+    // Find the button and trigger a click event
+    const addToCartButton = wrapper.find('.product-item button');
+    if (addToCartButton.exists()) {
+      await addToCartButton.trigger('click');
+    }
 
-        expect(firstProductItem.text()).toContain(firstProduct.name);
-        expect(firstProductItem.text()).toContain(firstProduct.description);
-        expect(firstProductItem.text()).toContain(firstProduct.price.toFixed(2));
-    });
-
-    it('calls addToCart method when button is clicked', async () => {
-        const addToCart = vi.spyOn(ShopPage.methods, 'addToCart');
-        const wrapper = shallowMount(ShopPage);
-        const firstProductButton = wrapper.find('.product-item button');
-
-        await firstProductButton.trigger('click');
-        expect(addToCart).toHaveBeenCalled();
-    });
+    // Assert that addToCart was called
+    expect(addToCartSpy).toHaveBeenCalled();
+  });
 });
